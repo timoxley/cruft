4ca0231b953f5aea3a4868694cbe8e1924027879
Tim <secoif@gmail.com>
2013-07-19 12:23:24 +0800

    More debugging info.

--- test/recursive.js
+++ test/recursive.js
@@ -12,11 +12,11 @@ test('it removes cruft', function(t) {
       if (err) return console.error('error', err)
       Cruft(__dirname + '/tmp', __dirname + '/../Readme.md', function(err, stats) {
         t.ifError(err, 'no error')
-        t.assert(stats.after < stats.before, 'reduces total size. before: '+stats.before+' after: ' + stats.after)
+        t.assert(stats.after < stats.before, 'reduces total size, before: '+stats.before+' after: ' + stats.after)
         t.assert(stats.files.length, 'lists files')
         t.assert(stats.files.some(function(file) {
           return file.indexOf('node_modules/hawk/node_modules/boom/images') !== -1
-        }), 'lists nested dependencies: searching for node_modules/hawk/node_modules/boom/images in:\n ' + stats.files.join('\n'))
+        }), 'lists nested dependencies')
       })
     })
   })
